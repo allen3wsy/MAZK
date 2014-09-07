92bfe5ded14de2fafde7e6fbc06336453f69c411
EvanZ <cszhangzhan@gmail.com>
2014-09-07 14:53:01 -0400

    add admin

---
+++ src/edu/controller/administrator/Administrator.java
@@ -0,0 +1,58 @@
+/*
+ * Administrator.java
+ * @author Zhan.
+ * Copyright (c) 2014 Company. All rights reserved. 
+ */
+package edu.controller.administrator;
+
+import edu.modernapp.beans.Agent;
+import edu.modernapp.beans.ContactHistory;
+import edu.modernapp.beans.Customer;
+
+public class Administrator {
+	public int addCustomer() {
+		return 0;
+	}
+	
+	public int addAgent() {
+		return 0;
+	}
+	
+	public int delCustomer(String cid) {
+		return 0;
+	}
+	
+	public int delAgent(String aid) {
+		return 0;
+	}
+	
+	public int updateCustomer(Customer customer) {
+		return 0;
+	}
+	
+	public int updateAgent(Agent agent) {
+		return 0;
+	}
+	
+	public int addHistory(ContactHistory history) {
+		return 0;
+	}
+	
+	public int delHistory(ContactHistory history) {
+		return 0;
+	}
+	
+	// obsolete because we used RDS instead.
+	public int load() {
+		return 0;
+	}
+	public int save() {
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
